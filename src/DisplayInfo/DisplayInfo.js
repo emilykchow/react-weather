01e6dc87c0922a5144c46342758406f94403e0a0
emilykchow <emilykchow@outlook.com>
2021-02-15 20:27:37 -0500

    time

--- src/DisplayInfo/DisplayInfo.js
+++ src/DisplayInfo/DisplayInfo.js
@@ -6,14 +6,18 @@ const DisplayInfo = ({weather}) => {
         return (null);
     }
 
+    var today = new Date();
+    const date = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
     return (
         <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 15) ? 'warmBg' : 'coldBg') : 'display-info-wrapper'}>
             <div className='display-content'>
                     <h3>{weather.name}, {weather.sys.country}</h3>
+                    <p>{date}</p>
                     <h4>Temperature: {Math.round(weather.main.temp)}°C</h4> 
-                    <h4>{weather.timezone}</h4>
-                    <h4>Low: {weather.main.temp_min}, High: {weather.main.temp_max}</h4> 
-                    <h4>Description: {weather.weather[0].description}</h4>
+                    <h4>Time: {weather.timezone}</h4>
+                    <h4>Low: {weather.main.temp_min}°C, High: {weather.main.temp_max}°C</h4> 
+                    <h4 className='capitalize'>Description: {weather.weather[0].description}</h4>
             </div>         
         </div>
        
